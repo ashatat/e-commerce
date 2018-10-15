49bcbf65ef9025b634257055998df2ecfec31058
FarahZaqout <farah.zaqout@gmail.com>
2018-10-15 16:53:02 +0300

    fixed linting errors in index.jsx and serviceWorker.js #26

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+/* global document */
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
