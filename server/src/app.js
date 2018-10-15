0edbf59c846eb47f40499eda8be98ce38fad561f
FarahZaqout <farah.zaqout@gmail.com>
2018-10-15 14:53:10 +0300

    adds body parser functionality to the express server #16

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
