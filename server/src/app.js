7c45ac747b50e7d5455c14044a1778e3c0ddcee7
FarahZaqout <farah.zaqout@gmail.com>
2018-10-15 15:03:50 +0300

    adds basic server functionality #16

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 3000);
+
+module.exports = app;
