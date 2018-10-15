587f7c753684780fa249c7295fbdc70ba0f69911
FarahZaqout <farah.zaqout@gmail.com>
2018-10-15 15:47:44 +0300

    replaced body parser with built in express body parser #24

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.set('port', process.env.PORT || 3000);
 
