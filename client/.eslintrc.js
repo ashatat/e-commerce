d26be880fa1231a34a0808b819c9e86b0b2a9748
abdalsamad <abdalsamad.y.m@gmail.com>
2018-10-19 17:32:57 +0300

    adding the pritter and pre-commit hooks relates #18 #44

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,3 +1,50 @@
 module.exports = {
-    "extends": "airbnb"
+  "extends": [
+    "airbnb",
+    "prettier",
+    "prettier/react"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "ecmaFeatures": {
+      "impliedStrict": true,
+      "classes": true
+    }
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  "rules": {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ],
+    "quotes": [
+      2,
+      "single",
+      {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "es5",
+        "singleQuote": true,
+        "printWidth": 80
+      }
+    ]
+  },
+  "plugins": [
+    "prettier"
+  ]
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,5 @@
 const app = require('./app.js');
 
-
 app.listen(app.get('port'), () => {
   console.log(`server is now rinning on port ${app.get('port')}`);
 });
