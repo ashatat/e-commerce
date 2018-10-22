23aaa10ee89d601114b2d2e23a3061e9cc58d653
abdalsamad <abdalsamad.y.m@gmail.com>
2018-10-22 21:47:57 +0300

    adding snapshot test to Tag component

--- client/src/components/common/Tag.test.js
+++ client/src/components/common/Tag.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
 import Tag from './Tag';
 
-test('Tag componet render', () => {
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Tag name="SALE" color="white" bgColor="#ea5f50" />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test('has a valid snapshot', () => {
   const component = renderer
     .create(<Tag name="SALE" color="white" bgColor="#ea5f50" />)
     .toJSON();
-
-  console.log(component);
   expect(component).toMatchSnapshot();
 });
