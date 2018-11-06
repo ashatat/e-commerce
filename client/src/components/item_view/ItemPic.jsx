8061d43e636abd73b52a19edc2954ae30060442f
ahmed shatat <a.shatat@hotmail.com>
2018-11-06 15:30:06 +0200

    add active thumbnail feature + fix eslint warnings #47

--- client/src/components/item_view/ItemPic.jsx
+++ client/src/components/item_view/ItemPic.jsx
@@ -1,94 +1,112 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './item-pic.css';
 
 class ItemPic extends Component {
-
   state = {
-    currentIndex:0,
-    translateValue:0
-  }
-  
-  slideWidth = (element) => {
-    if (element === 'current_img'){
-      return document.querySelector(`#img${this.state.currentIndex}`).clientWidth;
-    }
-    if (element === 'slider') {
-      let sliderWidth = document.querySelector(`#img${this.state.currentIndex}`).clientWidth;
-      this.props.picArr.forEach((item, index) => {
-        sliderWidth -= document.querySelector(`#img${index}`).clientWidth;
-      })
-      return sliderWidth;
-    }
-  }
+    currentIndex: 0,
+    translateValue: 0,
+  };
 
-  nextImg = () => {
-    if (this.state.currentIndex === this.props.picArr.length-1) {
-      this.setState({
-        currentIndex:0,
-        translateValue:0
-      })
-    } else {
-      this.setState({
-        currentIndex: this.state.currentIndex + 1,
-        translateValue: this.state.translateValue - this.slideWidth('current_img')
-      })
-    }
-  }
-
-  prevImg = () => {
-    if (this.state.currentIndex === 0) {
-      this.setState({
-        currentIndex:this.props.picArr.length-1,
-        translateValue:this.slideWidth('slider')
-      })
-    } else {
-      this.setState({
-        currentIndex: this.state.currentIndex - 1,
-        translateValue: this.state.translateValue + this.slideWidth('current_img')
-      })
-    }
-  }
-
-  componentDidMount () {
+  componentDidMount() {
     window.addEventListener('resize', () => {
       this.setState({
-        currentIndex:0,
-        translateValue:0
-      })
-    })
+        currentIndex: 0,
+        translateValue: 0,
+      });
+    });
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     window.removeEventListener('resize');
   }
+
+  slideWidth = element => {
+    const { currentIndex } = this.state;
+    const { picArr } = this.props;
+
+    let sliderWidth;
+
+    if (element === 'current_img') {
+      sliderWidth = document.querySelector(`#img${currentIndex}`).clientWidth;
+    }
+    if (element === 'slider') {
+      sliderWidth = document.querySelector(`#img${currentIndex}`).clientWidth;
+      picArr.forEach((item, index) => {
+        sliderWidth -= document.querySelector(`#img${index}`).clientWidth;
+      });
+    }
+    return sliderWidth;
+  };
+
+  nextImg = () => {
+    const { currentIndex, translateValue } = this.state;
+    const { picArr } = this.props;
+
+    if (currentIndex === picArr.length - 1) {
+      this.setState({
+        currentIndex: 0,
+        translateValue: 0,
+      });
+    } else {
+      this.setState({
+        currentIndex: currentIndex + 1,
+        translateValue: translateValue - this.slideWidth('current_img'),
+      });
+    }
+  };
+
+  prevImg = () => {
+    const { currentIndex, translateValue } = this.state;
+    const { picArr } = this.props;
+
+    if (currentIndex === 0) {
+      this.setState({
+        currentIndex: picArr.length - 1,
+        translateValue: this.slideWidth('slider'),
+      });
+    } else {
+      this.setState({
+        currentIndex: currentIndex - 1,
+        translateValue: translateValue + this.slideWidth('current_img'),
+      });
+    }
+  };
 
   render() {
     const { translateValue, currentIndex } = this.state;
     const { picArr } = this.props;
-    return(
+
+    return (
       <div className="item_pic">
         <div className="slider">
-          <div style={{transform: `translateX(${translateValue}px)`}} >
-            {picArr.map((picUrl, index) => {
+          <div style={{ transform: `translateX(${translateValue}px)` }}>
+            {picArr.map((pic, index) => (
               // replace img with ZoomImg compoenet when it's ready
-              return <img key={index} id={`img${index}`} src={picUrl} alt="item" />
-            })}
+              <img key={pic.id} id={`img${index}`} src={pic.url} alt="item" />
+            ))}
           </div>
           <div className="navigation">
-            <span onClick={this.prevImg}>&lt;</span>
-            <span onClick={this.nextImg}>&gt;</span>
+            <button type="button" onClick={this.prevImg}>
+              &lt;
+            </button>
+            <button type="button" onClick={this.nextImg}>
+              &gt;
+            </button>
           </div>
         </div>
         <ul className="thumbnails">
-          {picArr.map((picUrl, index) => {
-            return (
-              <li id={index} className={(index === currentIndex)? "active":""}>
-                <img key={index} src={picUrl} alt="thumbnail" />
-              </li>
-            ) 
-          })}
+          {picArr.map((pic, index) => (
+            <li
+              key={pic.id}
+              id={index}
+              className={index === currentIndex ? 'active' : ''}
+            >
+              <div className="overlay" />
+              <img src={pic.url} alt="thumbnail" />
+            </li>
+          ))}
         </ul>
       </div>
     );
@@ -96,6 +114,6 @@ class ItemPic extends Component {
 }
 
 ItemPic.propTypes = {
-  picArr: PropTypes.array
-}
+  picArr: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default ItemPic;
