12858c9719d8842e95b3eb7bfa87fc19296e7adb
FarahZaqout <farah.zaqout@gmail.com>
2018-11-09 19:43:03 +0200

    removed style from input field

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-
+import InputField from './components/common/InputField';
 /* eslint-disable  */
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <InputField className="input__account"/>
+      <InputField className="input__footer"/>
       </div>
     );
   }
