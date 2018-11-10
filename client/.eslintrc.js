5b5cfb3f6846651f93f32a8dd1d3ec4901068d42
FarahZaqout <farah.zaqout@gmail.com>
2018-11-11 01:12:22 +0200

    removes the global require condition from the eslint rules as it's irrelevant to react

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
   "rules": {
     // disables the windows/unix linebreak checks.
     "linebreak-style": 0,
-    "global-require": 0,
     "eslint linebreak-style": [0, "error", "windows"],
     //  allow .js extensions for JSX.
     "react/jsx-filename-extension": [
