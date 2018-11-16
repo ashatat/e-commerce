1230154465fa3ed6bf27ea6d9018c760f2d6efa1
ishak <ishak-3@hotmail.com>
2018-11-17 00:00:05 +0200

    fix conflict

--- client/src/components/common/InputField/index.jsx
+++ client/src/components/common/InputField/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -61,57 +60,3 @@ Input.defaultProps = {
 };
 
 export default Input;
-=======
-import React from 'react';
-import PropTypes from 'prop-types';
-
-// this line disables the windows end of line check for eslint.
-/* eslint linebreak-style: ["error", "windows"] */
-// declares propTypes validation.
-const propTypes = {
-  type: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  placeholder: PropTypes.string,
-  className: PropTypes.string.isRequired,
-  onChange: PropTypes.func,
-  onClick: PropTypes.func,
-};
-
-const Input = props => {
-  const {
-    type,
-    name,
-    value,
-    placeholder,
-    className,
-    onChange,
-    onClick,
-  } = props;
-
-  return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      name={name}
-      value={value}
-      className={className}
-      onChange={onChange}
-      onClick={onClick}
-    />
-  );
-};
-
-// assigns the propTypes to the component.
-Input.propTypes = propTypes;
-
-// specifies the default propType value when not explicitly required.
-Input.defaultProps = {
-  value: null,
-  placeholder: null,
-  onChange: null,
-  onClick: null,
-};
-
-export default Input;
->>>>>>> 6fc2af9b3b90994e55d24db2f4658a96798499b4
