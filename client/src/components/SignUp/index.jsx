e573d7299daff83fe143d6df0abcfb3194b67ff6
FarahZaqout <farah.zaqout@gmail.com>
2018-11-17 05:35:23 +0200

    changes the submit button value from signup to login

--- client/src/components/SignUp/index.jsx
+++ client/src/components/SignUp/index.jsx
@@ -64,7 +64,7 @@ export default class SignUp extends Component {
           <InputField
             className="sign-up-form__submit"
             type="submit"
-            value="Sign Up"
+            value="Login"
           />
           <InputField
             className="sign-up-form__checkbox"
