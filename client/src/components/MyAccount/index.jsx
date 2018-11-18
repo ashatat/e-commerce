8e9ed30d3f43cc04c51c1fa0ff91cad3ed71a346
FarahZaqout <farah.zaqout@gmail.com>
2018-11-18 16:16:26 +0200

    creates the ref stripe in the my-account component

--- client/src/components/MyAccount/index.jsx
+++ client/src/components/MyAccount/index.jsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import './MyAccount.css';
 
 export default function MyAccount() {
   return (
     <Fragment>
       <div className="my-account">
-        <div>
-          <h2 className="my-account__header">MyAccount</h2>
+        <div className="my-account__container">
+          <h2 className="my-account__header">My Account</h2>
           <div className="my-account__link-wrapper">
             <Link className="my-account__home-ref" to="/">
               Atelier
