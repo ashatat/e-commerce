0afbc197f836e87a13d5861359a61e4a832c100a
FarahZaqout <farah.zaqout@gmail.com>
2018-11-19 15:58:24 +0200

    changes my-account to stateful component and adds the event handlers for signup and login forms

--- client/src/components/MyAccount/index.jsx
+++ client/src/components/MyAccount/index.jsx
@@ -1,28 +1,97 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import SignUp from '../SignUp';
 import Login from '../Login';
 import './MyAccount.css';
 
-export default function MyAccount() {
-  return (
-    <Fragment>
-      <div className="my-account">
-        <div className="my-account__container">
-          <h2 className="my-account__header">My Account</h2>
-          <div className="my-account__link-wrapper">
-            <Link className="my-account__home-ref" to="/">
-              Atelier
-            </Link>
-            <p className="my-account__wrapper-text"> &gt; My Account</p>
+export default class MyAccount extends Component {
+  state = {
+    loginEmail: '',
+    loginPassword: '',
+    loginRememberMe: false,
+    signupEmail: '',
+    signupPassword: '',
+  };
+
+  handleLoginChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleLoginSubmit = e => {
+    const { loginEmail, loginPassword, loginRememberMe } = this.state;
+    const loginState = {
+      loginEmail,
+      loginPassword,
+      loginRememberMe,
+    };
+
+    fetch('/login', {
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(loginState),
+    });
+    e.preventDefault();
+  };
+
+  handleLoginClick = () => {
+    this.setState(prevState => ({
+      LoginRememberMe: !prevState.LoginRememberMe,
+    }));
+  };
+
+  handleSignUpChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSignUpSubmit = e => {
+    const { signupEmail, signupPassword } = this.state;
+    const signUpState = {
+      signupEmail,
+      signupPassword,
+    };
+
+    e.preventDefault();
+    fetch('/sign-up', {
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(signUpState),
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <div className="my-account">
+          <div className="my-account__container">
+            <h2 className="my-account__header">My Account</h2>
+            <div className="my-account__link-wrapper">
+              <Link className="my-account__home-ref" to="/">
+                Atelier
+              </Link>
+              <p className="my-account__wrapper-text"> &gt; My Account</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="my-account__forms-container">
-        <Login />
-        <SignUp />
-      </div>
-    </Fragment>
-  );
+        <div className="my-account__forms-container">
+          <div className="my-account__error-message">hello</div>
+          <Login
+            handleChange={this.handleLoginChange}
+            handleSubmit={this.handleLoginSubmit}
+            handleClick={this.handleLoginClick}
+          />
+          <SignUp
+            handleChange={this.handleSignUpChange}
+            handleSubmit={this.handleSignUpSubmit}
+          />
+        </div>
+      </Fragment>
+    );
+  }
 }
