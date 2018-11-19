9c16101ca4275a113d11405c9e985a65fa8adb78
FarahZaqout <farah.zaqout@gmail.com>
2018-11-19 15:56:51 +0200

    adds propType validation to login component

--- client/src/components/SignUp/index.jsx
+++ client/src/components/SignUp/index.jsx
@@ -1,67 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import InputField from '../common/InputField';
 import './SignUp.css';
 
-export default class SignUp extends Component {
-  state = {
-    email: '',
-    password: '',
-  };
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = e => {
-    fetch('/sign-up', {
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify(this.state),
-    });
-    e.preventDefault();
-  };
-
-  render() {
-    return (
-      <form className="sign-up-form" onSubmit={this.handleSubmit}>
-        <div className="sign-up-form__container">
-          <h4 className="sign-up-form__header">Not registered? No problem</h4>
-          <p className="sign-up-form__paragraph">
-            Creating an account with Joyn is quick and easy, and will allow you
-            to move through our checkout quicker. You can also store multiple
-            shipping addresses, gain access to your order history, and much
-            more.
-          </p>
+export default function SignUp(props) {
+  const { handleSubmit, handleChange } = props;
+  return (
+    <form className="sign-up-form" onSubmit={handleSubmit}>
+      <div className="sign-up-form__container">
+        <h4 className="sign-up-form__header">Not registered? No problem</h4>
+        <p className="sign-up-form__paragraph">
+          Creating an account with Joyn is quick and easy, and will allow you to
+          move through our checkout quicker. You can also store multiple
+          shipping addresses, gain access to your order history, and much more.
+        </p>
+        <InputField
+          id="email"
+          labelText="Email address"
+          className="sign-up-form__text"
+          labelClass="sign-up-form__label"
+          name="signupEmail"
+          type="text"
+          onChange={handleChange}
+        />
+        <InputField
+          id="password"
+          labelText="Password"
+          labelClass="sign-up-form__label"
+          className="sign-up-form__text"
+          name="signupPassword"
+          type="text"
+          onChange={handleChange}
+        />
+        <div className="sign-up-form__submit-container">
           <InputField
-            id="email"
-            labelText="Email address"
-            className="sign-up-form__text"
-            labelClass="sign-up-form__label"
-            name="email"
-            type="text"
-            onChange={this.handleChange}
+            className="sign-up-form__submit"
+            type="submit"
+            value="Register"
           />
-          <InputField
-            id="password"
-            labelText="Password"
-            labelClass="sign-up-form__label"
-            className="sign-up-form__text"
-            name="password"
-            type="text"
-            onChange={this.handleChange}
-          />
-          <div className="sign-up-form__submit-container">
-            <InputField
-              className="sign-up-form__submit"
-              type="submit"
-              value="Register"
-            />
-          </div>
         </div>
-      </form>
-    );
-  }
+      </div>
+    </form>
+  );
 }
+
+SignUp.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
